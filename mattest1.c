2a4f15d52bb26c059deeabcb9ed5bc080b5cb89b
Ellen-Co2 <coowsome@gmail.com>
2015-01-09 13:31:15 +0800

    add the repeat and broadcast

--- mattest1.c
+++ mattest1.c
@@ -4,6 +4,7 @@
 #include <math.h>
 #define m 6
 #define n 2
+#define rep 100
 //calculate A[m][n]*V[n][1]
 int main(int argc, char* argv[] )
 {
@@ -21,13 +22,17 @@ int main(int argc, char* argv[] )
     printf("Hello world from rank %d"
          " out of %d processors\n", world_rank, world_size);
     starttime = MPI_Wtime();
-
+//repeat the calculation for rep times
+  for (int i = 0; i < rep; i++)
+    {
+        /* code */
+      
     for(j = 0, V[j]=0; j < n; j++)
     { 
         V[j]=1;
         printf("V[%d]:%d \n",j,V[j]);
       //V[j] = rand() % 10;  
-      //printf("V[%d]:%d",j,V[j]);如果不指定向量的话，rand需要在proc 0定义向量再发送到各个进程
+      //printf("V[%d]:%d",j,V[j]);
     }
 
     if (world_rank == 0) 
@@ -36,7 +41,7 @@ int main(int argc, char* argv[] )
         for(i = 0; i < m; i++)
            for(j = 0; j < n; j++)
                {
-               A[i][j] = i * n + j; 
+               A[i][j] = i * n + j; //fix the matrix
                //A[i][j] = rand() % 100;  
                printf("A[%d][%d]:%d \n",i,j,A[i][j]); 
                }
@@ -65,24 +70,24 @@ int main(int argc, char* argv[] )
         for (i=1; i < world_size; i++)
         {
             if (i != world_size - 1)  
-                my_row_size = m/world_size;
+                my_row_size =int(m/world_size) ;
             if (i == world_size - 1) 
-                my_row_size = m - (world_size - 1) * (m/world_size);
+                my_row_size = m - (world_size - 1) * int(m/world_size);
             MPI_Recv(&C[(m/world_size)*i], my_row_size, MPI_INT, i,0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         }
         for (i=0; i < m ; i++)
             printf("Final : C[%d]:%d \n",i,C[i]);
 
     }
-    divid_time = MPI_Wtime();//distribute and initiate time
+    //divid_time = MPI_Wtime();//distribute and initiate time in the loop
     if (world_rank != 0) 
     {
         int my_row_size;
         if (i != world_size - 1) { 
-            my_row_size =m/world_size;
+            my_row_size =int(m/world_size);
         }        
         if (i == world_size - 1) { 
-            my_row_size = m - (world_size -1)*m/world_size;
+            my_row_size = m - (world_size -1)*(int(m/world_size));
         }
         int AA[my_row_size][n];
         int C[my_row_size];   
@@ -103,30 +108,19 @@ int main(int argc, char* argv[] )
                C[i]+=AA[i][j]*V[j];
             } 
             printf("C[%d]:%d \n",i,C[i]);
-        }//Send the computed matrix to every nodes用Allgather函数发送到所有进程？
+        }//Send the computed matrix to proc 0
 
         MPI_Send(&C[0], my_row_size, MPI_INT, 0,0, MPI_COMM_WORLD);
 
     }
-    /* 
-    if (world_rank != 0) 
-    {
-      number = world_rank + 100;
-      MPI_Send(&number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
-    } 
-    else if (world_rank == 0) 
-    {
-    for (i=1; i < world_size; i++)
-    {
-       MPI_Recv(&number, 1, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-       printf("Process 1 received number %d from process %d\n",
-              number, i);
-    }
-     }
-    */
+    MPI_Bcast(&C[0],n,MPI_INT,0,MPI_COMM_WORLD)
+    //check whether the C[n] is calculated
+    for(i=0,i<n,i++) printf("C[%d] %d \t", i , C[i] );
+}//end of repeat
+    
     endtime = MPI_Wtime();
     printf("Total TIME: %f seconds \n ", endtime-starttime);
-    printf("Calculation TIME: %f seconds \n ", endtime-divid_time);
+   // printf("Calculation TIME: %f seconds \n ", endtime-divid_time);
 
     MPI_Finalize();
 }
